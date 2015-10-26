59a83cadec4f4f795dbf0ac5776f7820675aa83f
Richard Good <Richard.Good@ed.ac.uk>
2015-10-26 12:30:13 +0000

    WEB008, set authentication manager in filter.

--- src/main/java/uk/ac/ed/notify/Application.java
+++ src/main/java/uk/ac/ed/notify/Application.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.security.oauth2.sso.EnableOAuth2Sso;
 import org.springframework.cloud.security.oauth2.sso.OAuth2SsoConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -46,13 +47,18 @@ public class Application extends SpringBootServletInitializer {
     @EnableOAuth2Sso
     public static class LoginConfigurer extends OAuth2SsoConfigurerAdapter {
 
+        @Autowired
+        AuthenticationManager authenticationManager;
+
         @Override
         public void configure(HttpSecurity http) throws Exception {
+            RemoteUserAuthenticationFilter filter = new RemoteUserAuthenticationFilter();
+            filter.setAuthenticationManager(authenticationManager);
             http.logout()
                     .and().httpBasic().disable()
                     .antMatcher("/**").authorizeRequests()
                     .antMatchers("/notification/**", "/user", "/favicon.ico", "/js/**", "/css/**", "/webjars/**", "/**/*.html").permitAll()
-                    .anyRequest().authenticated().and().csrf().disable().addFilter(new RemoteUserAuthenticationFilter());
+                    .anyRequest().authenticated().and().csrf().disable().addFilter(filter);
                    /* .csrfTokenRepository(csrfTokenRepository()).and()
                     .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);*/
         }
--- src/main/java/uk/ac/ed/notify/config/RemoteUserAuthenticationFilter.java
+++ src/main/java/uk/ac/ed/notify/config/RemoteUserAuthenticationFilter.java
@@ -25,4 +25,6 @@ public class RemoteUserAuthenticationFilter extends AbstractPreAuthenticatedProc
         return "N/A";
     }
 
+
+
 }
