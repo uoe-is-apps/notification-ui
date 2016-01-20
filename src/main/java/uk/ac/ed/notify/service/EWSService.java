5b84fe1bb4409c0ccba98b2359f046612b05e193
Michael Sun <hui.sun@ed.ac.uk>
2016-01-20 15:39:10 +0000

    blank place holder for EWSService

---
+++ src/main/java/uk/ac/ed/notify/service/EWSService.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.ed.notify.service;
+
+/**
+ *
+ * @author hsun1
+ */
+public class EWSService {
+    
+}
