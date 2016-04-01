4cbd2b4cd1f48b321688c253526881df910b1dec
Michael Sun <hui.sun@ed.ac.uk>
2016-04-01 15:39:29 +0100

    added DisallowConcurrentExecution to prevent job to be started when the previous one is still running

--- src/main/java/uk/ac/ed/notify/job/LearnPullJob.java
+++ src/main/java/uk/ac/ed/notify/job/LearnPullJob.java
@@ -1,5 +1,6 @@
 package uk.ac.ed.notify.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -13,6 +14,7 @@ import uk.ac.ed.notify.service.LearnService;
  * Job to implement the Learn pull
  * Created by rgood on 05/10/15.
  */
+@DisallowConcurrentExecution
 public class LearnPullJob implements Job {
     private static final Logger logger = LoggerFactory.getLogger(LearnPullJob.class);
      
