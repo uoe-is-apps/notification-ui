28bf516f3789ca8777d75fa354b6eb8e67464dff
Michael Sun <hui.sun@ed.ac.uk>
2018-05-07 11:19:06 +0100

    added delete statement to remove learn topic and learn publisher

--- src/main/resources/db/changelog/sql/delete_learn_job_oracle.sql
+++ src/main/resources/db/changelog/sql/delete_learn_job_oracle.sql
@@ -6,4 +6,8 @@ delete from QRTZ_TRIGGERS where trigger_name  in ('learnPullJobDetail','learnSys
 
 delete from QRTZ_JOB_DETAILS where job_name in ('learnPullJobDetail','learnSystemPullJobDetail');
 
+delete from TOPIC_DETAILS where topic_id in ('Learn System Announcement','Learn Course Announcement','Learn Assessment','Learn Task');
+
+delete from PUBLISHER_DETAILS where publisher_id in ('learn');
+
 commit;
